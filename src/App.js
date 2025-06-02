4238c41d115a4909dcb6cd0011f654ed8148bc47
Zaphie Zaps <zaphezulut@gmail.com>
2025-06-03 01:07:23 +0200

    Remove floating animated health/science icons with random motion

--- src/App.js
+++ src/App.js
@@ -9,15 +9,7 @@ function App() {
       <div className="background-image" style={{ backgroundImage: `url(${bgImage})` }}></div>
 
       {/* Floating animated health/science icons */}
-      <div className="floating-icons">
-        <span className="float-icon" style={{ top: '10%', left: '5%' }}>🧬</span>
-        <span className="float-icon" style={{ top: '30%', left: '70%' }}>💊</span>
-        <span className="float-icon" style={{ top: '60%', left: '20%' }}>🧠</span>
-        <span className="float-icon" style={{ top: '80%', left: '80%' }}>🧪</span>
-        <span className="float-icon" style={{ top: '50%', left: '40%' }}>💇</span>
-        <span className="float-icon" style={{ top: '25%', left: '15%' }}>🩺</span>
-      </div>
-
+      
       {/* Main app content */}
       <div className="App">
         <header className="App-header">Welcome to WellBlockz 💊</header>
